408d18573257006b6cd52bfd435f98192d3429ef
poorab21 <111352184+poorab21@users.noreply.github.com>
2023-04-10 08:25:17 -0700

    MaidIn

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,29 @@
 import '../styles/global.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { Atom } from 'react-loading-indicators'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  const [loading,setLoading] = useState(false)
   
+  useEffect(()=>{
+
+    router.events.on('routeChangeStart',()=>{
+        setLoading(true)
+    })
+
+    router.events.on('routeChangeComplete',()=>{
+      setLoading(false)
+    })
+  },[])
+  
+  if(loading) return <div className = {"spinnerContainer"}>
+        <Atom size = {'small'} color={'cornflowerblue'}  />
+    </div>
+
   return (
     <>
       <Head>
