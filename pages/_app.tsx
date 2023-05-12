92ecb782adbc0975c97cf38c0a5b4d3d1bfb8472
poorab21 <111352184+poorab21@users.noreply.github.com>
2023-05-11 21:22:40 -0700

    MaidIn

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,23 @@
 import '../styles/global.css'
+import * as React from 'react';
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import { Atom } from 'react-loading-indicators'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import theme from './theme';
+import createEmotionCache from './createEmotionCache';
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+export interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter()
@@ -27,13 +40,16 @@ export default function App({ Component, pageProps }: AppProps) {
     </div>
 
   return (
-    <>
+    <CacheProvider value={clientSideEmotionCache}>
       <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps}  />
-      </QueryClientProvider>
-    </>
+      <ThemeProvider theme = {theme}>
+        <CssBaseline/>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps}  />
+        </QueryClientProvider>
+      </ThemeProvider>
+    </CacheProvider>
     )
 }
